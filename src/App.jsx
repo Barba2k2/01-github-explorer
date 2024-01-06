f72bbde9d4fdc4cf2250b7688f80d48ec6873c22
Barbe2k2 <lorenzo.dz@hotmail.com>
2024-01-06 13:49:34 -0300

    started project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./styles/global.scss";
+import { RepositoryList } from "./components/RepositoryList";
+import { Counter } from "./components/Counter";
 
 export function App() {
-  return <h1>Hello ReactJS</h1>;
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  );
 }
